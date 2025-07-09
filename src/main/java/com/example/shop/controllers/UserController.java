5e40f1d94b0d64c27e79b602962802a1d95d45e2
anderson <anderdavid86@gmail.com>
2025-07-09 17:03:23 -0500

    prueba de ci 4

--- src/main/java/com/example/shop/controllers/UserController.java
+++ src/main/java/com/example/shop/controllers/UserController.java
@@ -17,24 +17,25 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<String> findAllUsers(){
-        //return repo.findAll();
-        return ResponseEntity.ok("prueba de ci 3");
+    public ResponseEntity<String> findAllUsers() {
+        // return repo.findAll();
+        return ResponseEntity.ok("prueba de ci 4");
     }
+
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id){
+    public User getUser(@PathVariable Long id) {
         return repo.findById(id).orElse(null);
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user){
+    public User createUser(@RequestBody User user) {
         return repo.save(user);
     }
 
     @PutMapping("/{id}")
-    public User updateUsuario(@PathVariable Long id, @RequestBody User user){
+    public User updateUsuario(@PathVariable Long id, @RequestBody User user) {
         User existUser = repo.findById(id).orElse(null);
-        if(existUser != null){
+        if (existUser != null) {
             existUser.setNombre(user.getNombre());
             existUser.setEdad(user.getEdad());
             repo.save(existUser);
@@ -43,7 +44,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id){
+    public void deleteUser(@PathVariable Long id) {
         repo.deleteById(id);
     }
 
